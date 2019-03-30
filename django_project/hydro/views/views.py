5c70d1c5eb5add32445b37dfb6d65334d34ecad9
Christiaanvdm <Christiaan@kartoza.com>
2019-03-30 20:56:09 +0200

    Added simple home apge with routing

--- django_project/hydro/views/views.py
+++ django_project/hydro/views/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render # noqa
+import json
+from django.shortcuts import (
+    render,
+    redirect,
+    get_object_or_404,
+    render_to_response
+)
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
